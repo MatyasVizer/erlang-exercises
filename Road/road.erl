6e0cc20df9a41a3303b1d61c6f59057b43469a45
MatyasVizer <matyasvizer@gmail.com>
2022-04-08 09:51:25 +0200

    upgrades added

--- Road/road.erl
+++ Road/road.erl
@@ -2,7 +2,7 @@
 -compile(export_all).
 
 main() ->
-    File="road.txt",
+    File = "road.txt",
     {ok, Bin} = file:read_file(File),
     parse_map(Bin).
 
@@ -10,6 +10,11 @@ main() ->
 parse_map(Bin) when is_binary(Bin) ->
     parse_map(binary_to_list(Bin));
 parse_map(Str) when is_list(Str) ->
-    [list_to_integer(X) || X <- string:tokens(Str,"\r\n\t ")].
+    Values = [list_to_integer(X) || X <- string:tokens(Str, "\r\n\t ")],
+    group_vals(Values, []).er
 
 
+group_vals([], Acc) ->
+    lists:reverse(Acc);
+group_vals([A, B, X | Rest], Acc) ->
+    group_vals(Rest, [{A, B, X} | Acc]).
